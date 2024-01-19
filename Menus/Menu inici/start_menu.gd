fe1badba068479dfd55eddb162435059fac80cb5
ElNico485 <nrs8689@claretbarcelona.cat>
2024-01-19 14:57:18 +0100

    Nivell 3 acabat + Nivell 4 + 5 +6

--- Menus/Menu inici/start_menu.gd
+++ Menus/Menu inici/start_menu.gd
@@ -13,8 +13,29 @@ func _on_start_game_button_pressed():
 	
 func _on_lvl2_button_pressed():
 	await LevelTransition.fade_to_black()
-	get_tree().change_scene_to_file("res://Nivells/Nivell 3/Level 3.tscn")
+	get_tree().change_scene_to_file("res://Nivells/Nivell 2/Level_two.tscn")
+	LevelTransition.fade_from_black()
+	
+func _on_lvl3_button_pressed():
+	await LevelTransition.fade_to_black()
+	get_tree().change_scene_to_file("res://Nivells/Nivell 3/level_three.tscn")
+	LevelTransition.fade_from_black()
+	
+func _on_lvl4_button_pressed():
+	await LevelTransition.fade_to_black()
+	get_tree().change_scene_to_file("res://Nivells/Nivell 4/level_four.tscn")
+	LevelTransition.fade_from_black()
+	
+func _on_lvl5_button_pressed():
+	await LevelTransition.fade_to_black()
+	get_tree().change_scene_to_file("res://Nivells/Nivell 5/level_five.tscn")
+	LevelTransition.fade_from_black()
+	
+func _on_lvl6_button_pressed():
+	await LevelTransition.fade_to_black()
+	get_tree().change_scene_to_file("res://Nivells/Nivell 6/level_six.tscn")
 	LevelTransition.fade_from_black()
 	
 func _on_quit_button_pressed():
 	get_tree().quit()
+
